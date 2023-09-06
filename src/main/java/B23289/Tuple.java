af2b79ff7a7904c17c23af86d1711cea5c6f3384
3juhwan <13selfesteem91@naver.com>
2023-09-06 15:53:03 +0900

    feat(23289): define tuple data-structure

---
+++ src/main/java/B23289/Tuple.java
@@ -0,0 +1,25 @@
+package main.java.B23289;
+
+public class Tuple<F, S> {
+
+    private final F first;
+    private final S second;
+
+    public Tuple(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static <F, S> Tuple<F, S> of(final F first, final S second) {
+        return new Tuple<>(first, second);
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+}
