6cff53dd17521181bb344c651a065eeb14fb3614
3juhwan <13selfesteem91@naver.com>
2023-09-06 15:53:46 +0900

    feat(23289): labeling direction

--- src/main/java/B23289/Direction.java
+++ src/main/java/B23289/Direction.java
@@ -1,17 +1,31 @@
 package main.java.B23289;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Direction {
 
-    UP(-1, 0),
-    DOWN(1, 0),
-    LEFT(0, -1),
-    RIGHT(0, 1);
+    UP(3, -1, 0),
+    RIGHT(1, 0, 1),
+    DOWN(4, 1, 0),
+    LEFT(2, 0, -1);
 
-    private final int dx, dy;
+    private static final Map<Integer, Direction> label = Stream.of(values()).collect(Collectors.toMap(Direction::getNum, e -> e));
+    private final int num, dx, dy;
 
-    Direction(int dx, int dy) {
+    Direction(int num, int dx, int dy) {
+        this.num = num;
         this.dx = dx;
         this.dy = dy;
+    }
+
+    public static Direction valueOfLabel(int num) {
+        return label.get(num);
+    }
+
+    public int getNum() {
+        return this.num;
     }
 
     public int getDx() {
